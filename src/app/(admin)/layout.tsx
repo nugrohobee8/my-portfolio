89f6ee40f57c7fd5930aec401988ba92f90acde0
nugrohobee8 <email-akun-nugrohobee8@gmail.com>
2026-07-27 19:45:53 +0700

    ui : add homepage

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,18 +1,5 @@
-import { ThemeToggle } from '@/components/theme-toggle'
-
 export default function AdminLayout({
     children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <>
-            <div className="absolute top-4 right-4">
-                <ThemeToggle />
-            </div>
-            <main className="flex-1">
-                {children}
-            </main>
-        </>
-    )
+}: Readonly<{ children: React.ReactNode }>) {
+    return <main className="flex-1">{children}</main>
 }
